0d48bffa8f6e84e17b986b84d73d0d34c18a843e
ARezaieZR <a.rezaiezr@gmail.com>
2019-07-06 10:33:32 +0430

    - No comment

--- sales-core/controller/src/main/java/com/nicico/sales/controller/BankRestController.java
+++ sales-core/controller/src/main/java/com/nicico/sales/controller/BankRestController.java
@@ -1,17 +1,25 @@
 package com.nicico.sales.controller;
 
+import com.nicico.copper.common.domain.ConstantVARs;
 import com.nicico.copper.core.dto.search.SearchDTO;
 import com.nicico.copper.core.util.Loggable;
+import com.nicico.copper.core.util.report.ReportUtil;
 import com.nicico.sales.dto.BankDTO;
 import com.nicico.sales.iservice.IBankService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,87 +27,98 @@ import java.util.List;
 @RequestMapping(value = "/api/bank")
 public class BankRestController {
 
-	private final IBankService bankService;
+    private final IBankService bankService;
+    private final ReportUtil reportUtil;
 
-	// ------------------------------s
+    // ------------------------------s
 
-	@Loggable
-	@GetMapping(value = "/{id}")
+    @Loggable
+    @GetMapping(value = "/{id}")
 //    @PreAuthorize("hasAuthority('r_bank')")
-	public ResponseEntity<BankDTO.Info> get(@PathVariable Long id) {
-		return new ResponseEntity<>(bankService.get(id), HttpStatus.OK);
-	}
+    public ResponseEntity<BankDTO.Info> get(@PathVariable Long id) {
+        return new ResponseEntity<>(bankService.get(id), HttpStatus.OK);
+    }
 
-	@Loggable
-	@GetMapping(value = "/list")
+    @Loggable
+    @GetMapping(value = "/list")
 //    @PreAuthorize("hasAuthority('r_bank')")
-	public ResponseEntity<List<BankDTO.Info>> list() {
-		return new ResponseEntity<>(bankService.list(), HttpStatus.OK);
-	}
+    public ResponseEntity<List<BankDTO.Info>> list() {
+        return new ResponseEntity<>(bankService.list(), HttpStatus.OK);
+    }
 
-	@Loggable
-	@PostMapping
+    @Loggable
+    @PostMapping
 //    @PreAuthorize("hasAuthority('c_bank')")
-	public ResponseEntity<BankDTO.Info> create(@Validated @RequestBody BankDTO.Create request) {
-		return new ResponseEntity<>(bankService.create(request), HttpStatus.CREATED);
-	}
+    public ResponseEntity<BankDTO.Info> create(@Validated @RequestBody BankDTO.Create request) {
+        return new ResponseEntity<>(bankService.create(request), HttpStatus.CREATED);
+    }
 
-	@Loggable
-	@PutMapping
+    @Loggable
+    @PutMapping
 //    @PreAuthorize("hasAuthority('u_bank')")
-	public ResponseEntity<BankDTO.Info> update(@RequestBody BankDTO.Update request) {
-		return new ResponseEntity<>(bankService.update(request.getId(), request), HttpStatus.OK);
-	}
+    public ResponseEntity<BankDTO.Info> update(@RequestBody BankDTO.Update request) {
+        return new ResponseEntity<>(bankService.update(request.getId(), request), HttpStatus.OK);
+    }
 
-	@Loggable
-	@DeleteMapping(value = "/{id}")
+    @Loggable
+    @DeleteMapping(value = "/{id}")
 //    @PreAuthorize("hasAuthority('d_bank')")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		bankService.delete(id);
-		return new ResponseEntity(HttpStatus.OK);
-	}
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        bankService.delete(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
-	@Loggable
-	@DeleteMapping(value = "/list")
+    @Loggable
+    @DeleteMapping(value = "/list")
 //    @PreAuthorize("hasAuthority('d_bank')")
-	public ResponseEntity<Void> delete(@Validated @RequestBody BankDTO.Delete request) {
-		bankService.delete(request);
-		return new ResponseEntity(HttpStatus.OK);
-	}
+    public ResponseEntity<Void> delete(@Validated @RequestBody BankDTO.Delete request) {
+        bankService.delete(request);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
-	@Loggable
-	@GetMapping(value = "/spec-list")
+    @Loggable
+    @GetMapping(value = "/spec-list")
 //    @PreAuthorize("hasAuthority('r_bank')")
-	public ResponseEntity<BankDTO.BankSpecRs> list(
-			@RequestParam("_startRow") Integer startRow,
-			@RequestParam("_endRow") Integer endRow,
-			@RequestParam(value = "operator", required = false) String operator,
-			@RequestParam(value = "criteria", required = false) String criteria
-	) {
-		SearchDTO.SearchRq request = new SearchDTO.SearchRq();
-		request.setStartIndex(startRow)
-				.setCount(endRow - startRow);
+    public ResponseEntity<BankDTO.BankSpecRs> list(
+            @RequestParam("_startRow") Integer startRow,
+            @RequestParam("_endRow") Integer endRow,
+            @RequestParam(value = "operator", required = false) String operator,
+            @RequestParam(value = "criteria", required = false) String criteria
+    ) {
+        SearchDTO.SearchRq request = new SearchDTO.SearchRq();
+        request.setStartIndex(startRow)
+                .setCount(endRow - startRow);
 
-		SearchDTO.SearchRs<BankDTO.Info> response = bankService.search(request);
+        SearchDTO.SearchRs<BankDTO.Info> response = bankService.search(request);
 
-		final BankDTO.SpecRs specResponse = new BankDTO.SpecRs();
-		specResponse.setData(response.getList())
-				.setStartRow(startRow)
-				.setEndRow(startRow + response.getTotalCount().intValue())
-				.setTotalRows(response.getTotalCount().intValue());
+        final BankDTO.SpecRs specResponse = new BankDTO.SpecRs();
+        specResponse.setData(response.getList())
+                .setStartRow(startRow)
+                .setEndRow(startRow + response.getTotalCount().intValue())
+                .setTotalRows(response.getTotalCount().intValue());
 
-		final BankDTO.BankSpecRs specRs = new BankDTO.BankSpecRs();
-		specRs.setResponse(specResponse);
+        final BankDTO.BankSpecRs specRs = new BankDTO.BankSpecRs();
+        specRs.setResponse(specResponse);
 
-		return new ResponseEntity<>(specRs, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(specRs, HttpStatus.OK);
+    }
 
-	// ------------------------------
+    // ------------------------------
 
-	@Loggable
-	@GetMapping(value = "/search")
+    @Loggable
+    @GetMapping(value = "/search")
 //    @PreAuthorize("hasAuthority('r_bank')")
-	public ResponseEntity<SearchDTO.SearchRs<BankDTO.Info>> search(@RequestBody SearchDTO.SearchRq request) {
-		return new ResponseEntity<>(bankService.search(request), HttpStatus.OK);
-	}
+    public ResponseEntity<SearchDTO.SearchRs<BankDTO.Info>> search(@RequestBody SearchDTO.SearchRq request) {
+        return new ResponseEntity<>(bankService.search(request), HttpStatus.OK);
+    }
+//---------------------------------------------------------------
+@Loggable
+@GetMapping(value = {"/print/{type}"})
+public void print(HttpServletResponse response, @PathVariable String type) throws SQLException, IOException, JRException {
+    Map<String, Object> params = new HashMap<>();
+    params.put(ConstantVARs.REPORT_TYPE, type);
+    reportUtil.export("/reports/Bank.jasper", params, response);
+}
+
+
 }
--- sales-web/web/src/main/java/com/nicico/sales/web/controller/BankFormController.java
+++ sales-web/web/src/main/java/com/nicico/sales/web/controller/BankFormController.java
@@ -1,21 +1,58 @@
 package com.nicico.sales.web.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletResponse;
-
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/bank")
 public class BankFormController {
 
-	@RequestMapping("/showForm")
-	public String showBank() {
-		return "base/bank";
-	}
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
-	@RequestMapping("/print/{type}")
-	public void printBank(HttpServletResponse response, @PathVariable String type) throws Exception {
-	}
+    @RequestMapping("/showForm")
+    public String showBank() {
+        return "base/bank";
+    }
+
+    @RequestMapping("/print/{type}")
+    public ResponseEntity<?> print(Authentication authentication, @PathVariable String type) {
+        String token = "";
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            OAuth2AuthorizedClient client = authorizedClientService
+                    .loadAuthorizedClient(
+                            ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId(),
+                            authentication.getName());
+            token = client.getAccessToken().getTokenValue();
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        if(type.equals("pdf"))
+            return restTemplate.exchange("http://localhost:9099/api/bank/print/pdf", HttpMethod.GET, entity, byte[].class);
+        else if(type.equals("excel"))
+            return restTemplate.exchange("http://localhost:9099/api/bank/print/excel", HttpMethod.GET, entity, byte[].class);
+        else if(type.equals("html"))
+            return restTemplate.exchange("http://localhost:9099/api/bank/print/html", HttpMethod.GET, entity, byte[].class);
+        else
+            return null;
+    }
 }
