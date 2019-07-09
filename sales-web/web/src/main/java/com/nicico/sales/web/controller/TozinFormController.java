67a74bc63594d8a786d4fb3a95db7b5a188c2366
abaspour <aa@aa>
2019-07-09 14:14:11 +0430

    report

--- sales-web/web/src/main/java/com/nicico/sales/web/controller/TozinFormController.java
+++ sales-web/web/src/main/java/com/nicico/sales/web/controller/TozinFormController.java
@@ -1,10 +1,16 @@
 package com.nicico.sales.web.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/tozin")
 public class TozinFormController {
 
