8ba7567c67a9e16a9d006d0af500af08fcd03e91
Reza <rmazloom88@gmail.com>
2019-08-06 15:58:16 +0430

    - Modified: fix logout

--- sales-web/web/src/main/java/com/nicico/sales/web/controller/HomeController.java
+++ sales-web/web/src/main/java/com/nicico/sales/web/controller/HomeController.java
@@ -25,54 +25,41 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/")
 public class HomeController {
 
-	private final OAuth2AuthorizedClientService authorizedClientService;
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
-	@Value("${nicico.oauth2.server.url}")
-	private String oauth2ServerUrl;
+    @Value("${nicico.oauth2.server.url}")
+    private String oauth2ServerUrl;
 
-	@GetMapping(value = {"/", "/home"})
-	public String showHomePage(Authentication authentication, HttpSession session) {
-		if (authentication instanceof OAuth2AuthenticationToken) {
-			OAuth2AuthorizedClient client = authorizedClientService
-					.loadAuthorizedClient(
-							((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId(),
-							authentication.getName());
-			try {
-				String token = client.getAccessToken().getTokenValue();
-				HttpHeaders headers = new HttpHeaders();
-				headers.add("Authorization", "Bearer " + token);
-				HttpEntity<String> request = new HttpEntity<String>(headers);
+    @GetMapping(value = {"/", "/home"})
+    public String showHomePage(Authentication authentication, HttpSession session) {
+        if (authentication instanceof OAuth2AuthenticationToken) {
+            OAuth2AuthorizedClient client = authorizedClientService
+                    .loadAuthorizedClient(
+                            ((OAuth2AuthenticationToken) authentication).getAuthorizedClientRegistrationId(),
+                            authentication.getName());
+            try {
+                String token = client.getAccessToken().getTokenValue();
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Authorization", "Bearer " + token);
+                session.setAttribute("token", token);
+            } catch (Exception e) {
+                return "underConstruction";
+            }
+        }
+        return "salesMainDesktop";
+    }
 
-				RestTemplate restTemplate = new RestTemplate();
+    @GetMapping("/oauth_login")
+    public String getLoginPage(Model model) {
+        if (!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+            return "redirect:/";
+        } else {
+            return "redirect:/oauth2/authorization/sso-login";
+        }
+    }
 
-//				ResponseEntity<String> processDiagram = restTemplate.exchange("http://localhost:9090/api/workflow/userTask/count/" + session.getAttribute("username"), HttpMethod.GET, request, String.class);
-//				session.setAttribute("cartableCount", processDiagram.getBody());
-				session.setAttribute("token", token);
-			} catch (Exception e) {
-				return "underConstruction";
-			}
-		}
-
-
-		return "salesMainDesktop";
-	}
-
-	@GetMapping(value = {"/login"})
-	public String showLoginPage() {
-		return "security/login";
-	}
-
-	@GetMapping("/oauth_login")
-	public String getLoginPage(Model model) {
-		if (!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken) && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
-			return "redirect:/";
-		} else {
-			return "redirect:/oauth2/authorization/sso-login";
-		}
-	}
-
-	@GetMapping("/oauth_logout")
-	public String logout() {
-		return "redirect:" + oauth2ServerUrl;
-	}
+    @GetMapping("/oauth_logout")
+    public String logout() {
+        return "redirect:" + oauth2ServerUrl;
+    }
 }
