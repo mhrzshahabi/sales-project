ef34fe65aa4be2cd8d4933e893edcfdde581876f
abaspour <aa@aa>
2019-09-25 13:54:08 +0330

    add fields to warehouselot table

--- model/src/main/java/com/nicico/sales/model/entities/base/WarehouseLot.java
+++ model/src/main/java/com/nicico/sales/model/entities/base/WarehouseLot.java
@@ -87,7 +87,7 @@ public class WarehouseLot extends Auditable {
 	private Double grossWeight;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "CONTRACT_ID", nullable = false, insertable = false, updatable = false)
+	@JoinColumn(name = "CONTRACT_ID", nullable = false, insertable = false, updatable = false,foreignKey = @ForeignKey(name = "WarehouseLotContract"))
 	private Contract contract;
 
 	@Column(name = "CONTRACT_ID")
