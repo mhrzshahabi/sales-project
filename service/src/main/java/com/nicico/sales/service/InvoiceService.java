d6e4f1c263c82b9ea86659c5065a3adf5fa396ff
abaspour <aa@aa>
2019-12-14 16:02:39 +0330

    bug fix invoice and rest

--- service/src/main/java/com/nicico/sales/service/InvoiceService.java
+++ service/src/main/java/com/nicico/sales/service/InvoiceService.java
@@ -1,7 +1,8 @@
 package com.nicico.sales.service;
 
+import com.nicico.copper.common.domain.criteria.NICICOCriteria;
 import com.nicico.copper.common.domain.criteria.SearchUtil;
-import com.nicico.copper.common.dto.search.SearchDTO;
+import com.nicico.copper.common.dto.grid.TotalResponse;
 import com.nicico.sales.SalesException;
 import com.nicico.sales.dto.InvoiceDTO;
 import com.nicico.sales.iservice.IInvoiceService;
@@ -96,8 +97,8 @@ public class InvoiceService implements IInvoiceService {
 
 	@Transactional(readOnly = true)
 	@Override
-	public SearchDTO.SearchRs<InvoiceDTO.Info> search(SearchDTO.SearchRq request) {
-		return SearchUtil.search(invoiceDAO, request, invoice -> modelMapper.map(invoice, InvoiceDTO.Info.class));
+	public TotalResponse<InvoiceDTO.Info> search(NICICOCriteria criteria) {
+		return SearchUtil.search(invoiceDAO, criteria, instruction -> modelMapper.map(instruction, InvoiceDTO.Info.class));
 	}
 
 	// ------------------------------
