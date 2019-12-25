b6f47bf258df63da2f8984d49dc4bc6549d8e300
Noushin Lotfi <A@A.com>
2019-12-25 14:24:12 +0330

    bug fix

--- web/src/main/java/com/nicico/sales/web/controller/ContractFormController.java
+++ web/src/main/java/com/nicico/sales/web/controller/ContractFormController.java
@@ -1,11 +1,8 @@
 package com.nicico.sales.web.controller;
 
-import com.nicico.copper.common.domain.ConstantVARs;
 import com.nicico.copper.core.util.file.FileUtil;
-import com.nicico.copper.core.util.report.ReportUtil;
 import com.nicico.sales.service.ContractService;
 import lombok.RequiredArgsConstructor;
-import net.sf.jasperreports.engine.JRException;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +15,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
@@ -39,31 +33,31 @@ public class ContractFormController {
     @RequestMapping("/print/{id}")
     public void print(HttpServletRequest request, HttpServletResponse response, @PathVariable Long id) throws IOException {
         String docName = contractService.printContract(id);
+        docName = docName + ".doc";
+        String UPLOAD_FILE_DIR = environment.getProperty("nicico.upload.dir");
+        String filePath = UPLOAD_FILE_DIR + File.separator + "contract" + File.separator + docName + ".doc";
+        File downloadFile = new File(filePath);
+        FileInputStream inputStream = new FileInputStream(downloadFile);
 
-            String UPLOAD_FILE_DIR = environment.getProperty("nicico.upload.dir");
-            String filePath = UPLOAD_FILE_DIR + File.separator + "contract" + File.separator + docName+".doc";
-            File downloadFile = new File(filePath);
-            FileInputStream inputStream = new FileInputStream(downloadFile);
+        ServletContext context = request.getServletContext();
+        String mimeType = context.getMimeType(filePath);
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
+        }
+        response.setContentType(mimeType);
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=%s", docName);
+        response.setHeader(headerKey, headerValue);
+        response.setContentLength((int) downloadFile.length());
+        OutputStream outputStream = response.getOutputStream();
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
 
-            ServletContext context = request.getServletContext();
-            String mimeType = context.getMimeType(filePath);
-            if (mimeType == null) {
-                mimeType = "application/octet-stream";
-            }
-            response.setContentType(mimeType);
-            String headerKey = "Content-Disposition";
-            String headerValue = String.format("attachment; filename=%s", docName);
-            response.setHeader(headerKey, headerValue);
-            response.setContentLength((int) downloadFile.length());
-            OutputStream outputStream = response.getOutputStream();
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            outputStream.flush();
-            inputStream.close();
-            fileUtil.download(docName, response);
+        outputStream.flush();
+        inputStream.close();
+        fileUtil.download(docName, response);
     }
 }
