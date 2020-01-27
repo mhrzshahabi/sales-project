28518bb36aba82d424b630c9ecaa91f938032a16
Reza <rmazloom88@gmail.com>
2020-01-27 15:28:58 +0330

    cathod list updated by view
    Signed-off-by: Reza <rmazloom88@gmail.com>

--- model/src/main/java/com/nicico/sales/model/entities/base/CatodList.java
+++ model/src/main/java/com/nicico/sales/model/entities/base/CatodList.java
@@ -1,48 +1,49 @@
 package com.nicico.sales.model.entities.base;
 
-import com.nicico.sales.model.Auditable;
-import lombok.*;
-import lombok.experimental.Accessors;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Subselect;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Accessors(chain = true)
 @EqualsAndHashCode(of = {"id"}, callSuper = false)
 @Entity
-@Subselect("select * from tbl_catod_list")
+@Subselect("select * from N_MASTER.V_CATODLISTFOREXPORT")
 public class CatodList {
 
-	@Id
-	@Column(name = "ID")
-	private Long id;
+    @Id
+    @Column(name = "ID")
+    private Long id;
 
-	@Column(name = "STOREID")
-	private String storeId;
+    @Column(name = "STOREID")
+    private String storeId;
 
-	@Column(name = "TOZINEID")
-	private String tozinId;
+    @Column(name = "TOZINEID")
+    private String tozinId;
 
-	@Column(name = "PRODUCTID")
-	private String productId;
+    @Column(name = "PRODUCTID")
+    private String productId;
 
-	@Column(name = "PRODUCTLABEL")
-	private String productLabel;
+    @Column(name = "PRODUCTLABEL")
+    private String productLabel;
 
-	@Column(name = "WAZN")
-	private Long wazn;
+    @Column(name = "WAZN")
+    private Long wazn;
 
-	@Column(name = "SHEETNUMBER")
-	private Long sheetNumber;
+    @Column(name = "SHEETNUMBER")
+    private Long sheetNumber;
 
-	@Column(name = "PACKINGTYPEID")
-	private Long packingTypeId;
+    @Column(name = "PACKINGTYPEID")
+    private Long packingTypeId;
 
-	@Column(name = "GDSCODE")
-	private Long gdsCode;
+    @Column(name = "GDSCODE")
+    private Long gdsCode;
 
 }
