8338ae389ba07f3e3446bf5673b8bf7e3a98a1e6
saeb <me@saeb.me>
2020-04-15 08:33:07 +0430

    model

---
+++ model/src/main/java/com/nicico/sales/model/entities/base/InvoiceExport.java
@@ -0,0 +1,41 @@
+package com.nicico.sales.model.entities.base;
+
+import com.nicico.sales.model.Auditable;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Entity
+@Table(name = "TBL_INVOICE_EXPORT")
+public class InvoiceExport extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_INVOICE_EXPORT")
+    @SequenceGenerator(name = "SEQ_INVOICE_EXPORT", sequenceName = "SEQ_INVOICE_EXPORT", allocationSize = 1)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "d_invoice_date")
+    private Date invoiceDate;
+
+    @ManyToOne
+    @Column(name = "n_shipment_id", insertable = false, updatable = false)
+    private Shipment shipment;
+
+    @NotNull
+    @Column(name = "n_shipment_id")
+    private Long shipmentId;
+
+    @Column(name = 'b_is_deleted')
+    private Boolean isDeleted;
+
+}
