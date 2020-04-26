ed23e2b24bf754979823c30ffc1e14f3a849b297
mshahabi <m@m.com>
2020-04-26 14:29:04 +0430

    Vessel Added

---
+++ model/src/main/java/com/nicico/sales/model/entities/base/Vessel.java
@@ -0,0 +1,40 @@
+package com.nicico.sales.model.entities.base;
+
+import com.nicico.sales.model.Auditable;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@Entity
+@Table(name = "TBL_VESSEL")
+public class Vessel extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_VESSEL")
+    @SequenceGenerator(name = "SEQ_VESSEL", sequenceName = "SEQ_VESSEL", allocationSize = 1)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "TYPE")
+    private String type;
+
+    @Column(name = "IMO")
+    private Long imo;
+
+    @Column(name = "YEAR_OF_BUILd")
+    private Long yearOfBuild;
+
+    @Column(name = "LENGTH")
+    private Double length;
+
+    @Column(name = "BEAM")
+    private Double beam;
+
+}
