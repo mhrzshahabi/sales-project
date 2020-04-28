6700d3f9190b3475f78ce02e1870a8bee8b71de0
ali <alikarimi1989@yahoo.com>
2020-04-28 22:37:08 +0430

    Feature: Contract Create initial models

--- web/src/main/java/com/nicico/sales/web/controller/contract/ContractFormController2.java
+++ web/src/main/java/com/nicico/sales/web/controller/contract/ContractFormController2.java
@@ -15,9 +15,9 @@ public class ContractFormController2 {
     @RequestMapping("/show-form")
     public String show(HttpServletRequest request) {
 
-        request.setAttribute("c_entity", SecurityUtil.hasAuthority("C_CONTRACT"));
-        request.setAttribute("u_entity", SecurityUtil.hasAuthority("U_CONTRACT"));
-        request.setAttribute("d_entity", SecurityUtil.hasAuthority("D_CONTRACT"));
+        request.setAttribute("c_entity", SecurityUtil.hasAuthority("C_CONTRACT2"));
+        request.setAttribute("u_entity", SecurityUtil.hasAuthority("U_CONTRACT2"));
+        request.setAttribute("d_entity", SecurityUtil.hasAuthority("D_CONTRACT2"));
         return "contract2/contract";
     }
 }
