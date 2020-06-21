3b8c46c8393c445eef5084faf27aa9c168db1ae3
Reza <rmazloom88@gmail.com>
2020-06-21 10:33:09 +0430

    bug fix
    Signed-off-by: Reza <rmazloom88@gmail.com>

--- model/src/main/java/com/nicico/sales/model/entities/base/TypicalAssay.java
+++ model/src/main/java/com/nicico/sales/model/entities/base/TypicalAssay.java
@@ -2,9 +2,10 @@ package com.nicico.sales.model.entities.base;
 
 
 import com.nicico.sales.model.Auditable;
-import com.nicico.sales.model.entities.warehouse.ItemElement;
+import com.nicico.sales.model.entities.warehouse.MaterialElement;
 import lombok.*;
 import lombok.experimental.Accessors;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -26,28 +27,28 @@ public class TypicalAssay extends Auditable {
     @Column(name = "ID")
     private Long id;
 
-    @Column(name = "N_MIN_VALUE" , precision = 5 , scale = 10 , nullable = false)
+    @Column(name = "N_MIN_VALUE", precision = 5, scale = 10, nullable = false)
     private BigDecimal minValue;
 
-    @Column(name = "N_MAX_VALUE" , precision = 5 , scale = 10 , nullable = false)
+    @Column(name = "N_MAX_VALUE", precision = 5, scale = 10, nullable = false)
     private BigDecimal maxValue;
 
     @Setter(AccessLevel.NONE)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "F_UNIT_ID" , insertable = false , updatable = false , foreignKey = @ForeignKey(name = "fk_typicalAssay2unitByUnitId"))
+    @JoinColumn(name = "F_UNIT_ID", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_typicalAssay2unitByUnitId"))
     private Unit unit;
 
     @NotNull
-    @Column(name = "F_UNIT_ID" , nullable = false)
+    @Column(name = "F_UNIT_ID", nullable = false)
     private Long unitId;
 
     @Setter
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "F_ITEM_ELEMENT_ID" , insertable = false , updatable = false , foreignKey =  @ForeignKey(name = "fk_typicalAssay2itemElementByItemElementId"))
-    private ItemElement itemElement;
+    @JoinColumn(name = "F_MATERIAL_ELEMENT_ID", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_typicalAssay2materialElementByMaterialElementId"))
+    private MaterialElement materialElement;
 
     @NotNull
-    @Column(name = "F_ITEM_ELEMENT_ID" , nullable = false)
-    private Long itemElementId;
+    @Column(name = "F_MATERIAL_ELEMENT_ID", nullable = false)
+    private Long materialElementId;
 }
 
