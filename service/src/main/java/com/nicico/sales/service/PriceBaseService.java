bce654e91b94ab64316292d5744235b3335b2dad
ali karimi <ali karimi>
2020-06-28 08:29:47 +0430

    - MODEL: ForeignInvoice

--- service/src/main/java/com/nicico/sales/service/PriceBaseService.java
+++ service/src/main/java/com/nicico/sales/service/PriceBaseService.java
@@ -7,15 +7,22 @@ import com.nicico.sales.repository.PriceBaseDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class PriceBaseService extends GenericService<PriceBase, Long, PriceBaseDTO.Create, PriceBaseDTO.Info, PriceBaseDTO.Update, PriceBaseDTO.Delete> implements IPriceBaseService {
-    private final PriceBaseDAO priceBaseDAO;
 
     @Override
-    public List<PriceBase> findAllByPriceBaseMonth(Integer year, Integer month) {
-        return priceBaseDAO.findAllByPriceBaseMonth(year, month);
+    public BigDecimal calculateElementPrice(Integer year, Integer month, Long elementId) {
+
+        List<PriceBase> prices = ((PriceBaseDAO) repository).getAllPrices(year, month, elementId);
+        BigDecimal averagePrice = BigDecimal.ZERO;
+        if (prices.size() > 0)
+            averagePrice = prices.stream().map(PriceBase::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add).divide(BigDecimal.valueOf(prices.size()), RoundingMode.HALF_EVEN);
+
+        return averagePrice;
     }
 }
