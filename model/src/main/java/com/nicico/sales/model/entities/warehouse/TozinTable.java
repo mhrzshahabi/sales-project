a76e67109802b3a83432a37e57e13ee88756c819
saeb <saeb.m.13@gmail.com>
2020-07-04 12:09:09 +0430

    baseentity for TozinTable

--- model/src/main/java/com/nicico/sales/model/entities/warehouse/TozinTable.java
+++ model/src/main/java/com/nicico/sales/model/entities/warehouse/TozinTable.java
@@ -1,6 +1,7 @@
 package com.nicico.sales.model.entities.warehouse;
 
 
+import com.nicico.sales.model.entities.common.BaseEntity;
 import lombok.*;
 import lombok.experimental.Accessors;
 
@@ -16,7 +17,7 @@ import javax.validation.constraints.NotNull;
 @EqualsAndHashCode(of = {"id"}, callSuper = false)
 @Entity
 @Table(name = "TBL_WARH_TOZIN")
-public class TozinTable {
+public class TozinTable extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_WARH_TOZIN")
     @SequenceGenerator(name = "SEQ_WARH_TOZIN", sequenceName = "SEQ_WARH_TOZIN", allocationSize = 1)
