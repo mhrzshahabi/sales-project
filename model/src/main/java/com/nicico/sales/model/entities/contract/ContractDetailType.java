c3546fa933daeab50f2a1d6738bdf1d8a1446e7d
Reza <rmazloom88@gmail.com>
2020-07-06 13:02:41 +0430

    test
    Signed-off-by: Reza <rmazloom88@gmail.com>

--- model/src/main/java/com/nicico/sales/model/entities/contract/ContractDetailType.java
+++ model/src/main/java/com/nicico/sales/model/entities/contract/ContractDetailType.java
@@ -1,11 +1,13 @@
 package com.nicico.sales.model.entities.contract;
 
+import com.nicico.sales.model.entities.base.Material;
 import com.nicico.sales.model.entities.common.BaseEntity;
 import lombok.*;
 import lombok.experimental.Accessors;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
@@ -23,12 +25,18 @@ public class ContractDetailType extends BaseEntity {
     @SequenceGenerator(name = "SEQ_CNTR_CONTRACT_DETAIL_TYPE", sequenceName = "SEQ_CNTR_CONTRACT_DETAIL_TYPE", allocationSize = 1)
     private Long id;
 
-    @Column(name = "C_MATERIAL", nullable = false, unique = true)
-    private Long material;
-
     @NotEmpty
-    @Column(name = "C_CODE", nullable = false, unique = true)
+    @Column(name = "C_CODE", nullable = false)
     private String code;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "F_MATERIAL_ID", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "FK_ContractDetailType2MaterialByMaterialId"))
+    private Material material;
+
+    @NotNull
+    @Column(name = "F_MATERIAL_ID", nullable = false)
+    private Long materialId;
 
     @NotEmpty
     @Column(name = "C_TITLE_FA", nullable = false)
@@ -38,9 +46,9 @@ public class ContractDetailType extends BaseEntity {
     @Column(name = "C_TITLE_EN", nullable = false)
     private String titleEn;
 
-    @OneToMany(mappedBy = "contractDetailType", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "contractDetailType", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<ContractDetailTypeParam> contractDetailTypeParams;
 
-    @OneToMany(mappedBy = "contractDetailType", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "contractDetailType", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ContractDetailTypeTemplate> contractDetailTypeTemplates;
 }
