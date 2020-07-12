a6ba4a0dab7f9e0899ff1207c4f14ad9d608ff32
ali karimi <ali karimi>
2020-07-12 09:53:35 +0430

    Revert "SALESJ-519"
    This reverts commit f8daa54b

--- web/src/main/resources/META-INF/resources/WEB-INF/views/invoice/foreign/js/component-invoice-payment.js
+++ web/src/main/resources/META-INF/resources/WEB-INF/views/invoice/foreign/js/component-invoice-payment.js
@@ -8,47 +8,74 @@ isc.defineClass("InvoicePayment", isc.VLayout).addProperties({
     membersMargin: 2,
     overflow: "scroll",
     currency: null,
+    contract: null,
     invoiceDeductionComponent: null,
+    invoiceBaseWeightComponent: null,
     invoiceCalculationComponent: null,
     initWidget: function () {
 
         this.Super("initWidget", arguments);
 
-        let fields = [];
-        fields.add(isc.Unit.create({
-            unitCategory: 1,
-            disabledUnitField: true,
-            disabledValueField: true,
-            disabledCurrencyField: true,
-            showValueFieldTitle: true,
-            showUnitFieldTitle: false,
-            showCurrencyFieldTitle: false,
-            showUnitField: false,
-            name: "unitPrice",
-            fieldValueTitle: '<spring:message code="foreign-invoice.form.unit-price"/>',
-            border: "1px solid rgba(0, 0, 0, 0.3)",
-        }));
-        fields.last().setUnitId(this.currency.symbol);
-        fields.last().setValue(this.invoiceCalculationComponent.getSumValue() - this.invoiceDeductionComponent.getSumValue());
-        fields.add(isc.Unit.create({
-            unitCategory: 1,
-            disabledUnitField: true,
-            disabledValueField: true,
-            disabledCurrencyField: true,
-            showValueFieldTitle: true,
-            showUnitFieldTitle: false,
-            showCurrencyFieldTitle: false,
-            showUnitField: false,
-            name: "sumFIPrice",
-            fieldValueTitle: '<spring:message code="foreign-invoice.form.unit-price"/>',
-            border: "1px solid rgba(0, 0, 0, 0.3)",
-        }));
-        fields.last().setUnitId(this.currency.symbol);
-        fields.last().setValue(this.invoiceCalculationComponent.getSumValue() - this.invoiceDeductionComponent.getSumValue());
+        let This = this;
+        isc.RPCManager.sendRequest(Object.assign(BaseRPCRequest, {
+            httpMethod: "GET",
+            actionURL: "${contextPath}/api/provisional-invoice/get-by-contract/" + This.contract.id,
+            callback: function (resp) {
 
-        This.addMember(isc.DynamicForm.create({
-            width: "100%",
-            fields: fields
+                let fields = [];
+                let unitPrice = This.invoiceCalculationComponent.getSumValue() - This.invoiceDeductionComponent.getSumValue();
+                let sumFIPrice = unitPrice * This.invoiceBaseWeightComponent.getValues().weightND;
+                fields.add(isc.Unit.create({
+                    unitCategory: 1,
+                    disabledUnitField: true,
+                    disabledValueField: true,
+                    disabledCurrencyField: true,
+                    showValueFieldTitle: true,
+                    showUnitFieldTitle: false,
+                    showCurrencyFieldTitle: false,
+                    showUnitField: false,
+                    name: "unitPrice",
+                    fieldValueTitle: '<spring:message code="foreign-invoice.form.unit-price"/>',
+                    border: "1px solid rgba(0, 0, 0, 0.3)",
+                }));
+                fields.last().setValue(unitPrice);
+                fields.last().setUnitId(This.currency.symbol);
+                fields.add(isc.Unit.create({
+                    unitCategory: 1,
+                    disabledUnitField: true,
+                    disabledValueField: true,
+                    disabledCurrencyField: true,
+                    showValueFieldTitle: true,
+                    showUnitFieldTitle: false,
+                    showCurrencyFieldTitle: false,
+                    showUnitField: false,
+                    name: "sumFIPrice",
+                    fieldValueTitle: '<spring:message code="foreign-invoice.form.sum-fi-price"/>',
+                    border: "1px solid rgba(0, 0, 0, 0.3)",
+                }));
+                fields.last().setValue(sumFIPrice);
+                fields.last().setUnitId(This.currency.symbol);
+                fields.add(isc.Unit.create({
+                    unitCategory: 1,
+                    disabledUnitField: true,
+                    disabledValueField: true,
+                    disabledCurrencyField: true,
+                    showValueFieldTitle: true,
+                    showUnitFieldTitle: false,
+                    showCurrencyFieldTitle: false,
+                    showUnitField: false,
+                    name: "sumPIPrice",
+                    fieldValueTitle: '<spring:message code="foreign-invoice.form.sum-pi-price"/>',
+                    border: "1px solid rgba(0, 0, 0, 0.3)",
+                }));
+                fields.last().setValue(sumPIPrice * -1);
+                fields.last().setUnitId(This.currency.symbol);
+
+                This.addMember(isc.DynamicForm.create({
+                    width: "100%",
+                    fields: fields
+                }));
+            }
         }));
     }
 });
