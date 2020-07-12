30a6beff8eb37b06cfc847304f91413f53eba686
ali karimi <ali karimi>
2020-07-12 11:12:46 +0430

    Revert "SALESJ-519"
    This reverts commit f8daa54b

--- web/src/main/resources/META-INF/resources/WEB-INF/views/invoice/foreign/js/component-invoice-payment.js
+++ web/src/main/resources/META-INF/resources/WEB-INF/views/invoice/foreign/js/component-invoice-payment.js
@@ -26,6 +26,7 @@ isc.defineClass("InvoicePayment", isc.VLayout).addProperties({
                 let unitPrice = This.invoiceCalculationComponent.getSumValue() - This.invoiceDeductionComponent.getSumValue();
                 let sumFIPrice = unitPrice * This.invoiceBaseWeightComponent.getValues().weightND;
                 fields.add(isc.Unit.create({
+                    isResult: false,
                     unitCategory: 1,
                     disabledUnitField: true,
                     disabledValueField: true,
@@ -40,7 +41,9 @@ isc.defineClass("InvoicePayment", isc.VLayout).addProperties({
                 }));
                 fields.last().setValue(unitPrice);
                 fields.last().setUnitId(This.currency.symbol);
+
                 fields.add(isc.Unit.create({
+                    isResult: true,
                     unitCategory: 1,
                     disabledUnitField: true,
                     disabledValueField: true,
@@ -55,6 +58,46 @@ isc.defineClass("InvoicePayment", isc.VLayout).addProperties({
                 }));
                 fields.last().setValue(sumFIPrice);
                 fields.last().setUnitId(This.currency.symbol);
+
+                let piValues = JSON.parse(resp.data);
+                for (let index = 0; index < piValues.length; index++) {
+
+                    fields.add(isc.Unit.create({
+                        isResult: true,
+                        unitCategory: 1,
+                        disabledUnitField: true,
+                        disabledValueField: true,
+                        disabledCurrencyField: true,
+                        showValueFieldTitle: true,
+                        showUnitFieldTitle: false,
+                        showCurrencyFieldTitle: false,
+                        showUnitField: false,
+                        piData: piValues[index],
+                        name: "sumPIPrice_" + piValues[index].no,
+                        fieldValueTitle: '<spring:message code="foreign-invoice.form.sum-pi-price"/>',
+                        border: "1px solid rgba(0, 0, 0, 0.3)",
+                        hoverHTML: function (item, form) {
+                            return `<div style="font-weight: bolder;">Provisional invoice NO. ` + item.piData.no + `</div>
+                                    <div>` + item.piData.description + `</div>`;
+                        }
+                    }));
+                    fields.last().setValue(piValues[index].sumPrice * -1);
+                    fields.last().setUnitId(piValues[index].currency.symbol);
+                }
+
+                let valuesForm = isc.DynamicForm.create({
+                    width: "100%",
+                    fields: fields
+                });
+                This.addMember(valuesForm);
+
+                let otherValuesGrid = isc.ListGrid.create({
+                });
+                This.addMember(otherValuesGrid);
+
+                fields = [];
+                let sumOfVOtherValuesGrid = otherValuesGrid.getData().map(q => q.finalValue).sum();
+                let sumOfValuesForm = valuesForm.fields.filter(q => q.isResult).map(q => q.getValue()).sum();
                 fields.add(isc.Unit.create({
                     unitCategory: 1,
                     disabledUnitField: true,
@@ -64,12 +107,32 @@ isc.defineClass("InvoicePayment", isc.VLayout).addProperties({
                     showUnitFieldTitle: false,
                     showCurrencyFieldTitle: false,
                     showUnitField: false,
-                    name: "sumPIPrice",
-                    fieldValueTitle: '<spring:message code="foreign-invoice.form.sum-pi-price"/>',
+                    name: "sumPrice",
+                    fieldValueTitle: '<spring:message code="foreign-invoice.form.sum-price"/>',
                     border: "1px solid rgba(0, 0, 0, 0.3)",
                 }));
-                fields.last().setValue(sumPIPrice * -1);
                 fields.last().setUnitId(This.currency.symbol);
+                fields.last().setValue(sumOfValuesForm + sumOfVOtherValuesGrid);
+
+                fields.add({
+                    name: "curDate",
+                    title: "<spring:message code='currencyRate.curDate'/>",
+                    type: "date",
+                    width: "400",
+                    required: true,
+                    validators: [
+                        {
+                            type: "required",
+                            validateOnChange: true
+                        }]
+                });
+                fields.add({
+                    width: "20%",
+                    name: "conversionDate",
+                    required: true,
+                    valueMap: JSON.parse('${Enum_DataType}'),
+                    title: "<spring:message code='global.type'/>"
+                });
 
                 This.addMember(isc.DynamicForm.create({
                     width: "100%",
