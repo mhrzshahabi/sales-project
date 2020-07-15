647f54fecc670a1babd25fec8468ca04f989e87b
jazad <jazad0@gmail.com>
2020-07-15 09:11:17 +0430

    Currency Rate - Fix

--- web/src/main/java/com/nicico/sales/web/controller/UnitFormController.java
+++ web/src/main/java/com/nicico/sales/web/controller/UnitFormController.java
@@ -4,13 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nicico.copper.core.SecurityUtil;
 import com.nicico.sales.model.enumeration.CategoryUnit;
+import com.nicico.sales.model.enumeration.SymbolUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,6 +24,12 @@ public class UnitFormController {
 
     @RequestMapping("/show-form")
     public String showUnit(HttpServletRequest request) throws JsonProcessingException {
+
+        request.setAttribute("Enum_SymbolUnit_WithValue", objectMapper.writeValueAsString(
+                Arrays.stream(SymbolUnit.values()).
+                        collect(Collectors.
+                                toMap(SymbolUnit::name, SymbolUnit::name)))
+        );
 
         Map<String, String> categoryUnit = new HashMap<>();
         for (CategoryUnit value : CategoryUnit.values()) categoryUnit.put(value.name(), value.name());
--- web/src/main/resources/META-INF/resources/WEB-INF/views/base/currencyRate/js/currencyRate.js
+++ web/src/main/resources/META-INF/resources/WEB-INF/views/base/currencyRate/js/currencyRate.js
@@ -9,7 +9,7 @@ currencyRateTab.dynamicForm.fields = BaseFormItems.concat([
         name: "currencyDate",
         title: "<spring:message code='currencyRate.curDate'/>",
         type: "date",
-        format: 'DD-MM-YYYY',
+        format: "DD-MM-YYYY",
         width: "100%",
         required: true,
     },
@@ -18,30 +18,41 @@ currencyRateTab.dynamicForm.fields = BaseFormItems.concat([
         title: "<spring:message code='currency.rate.f'/>",
         required: true,
         width: "100%",
-        valueMap : JSON.parse('${Enum_SymbolCUR}'),
+        valueMap: JSON.parse("${Enum_SymbolCUR}"),
     },
     {
         name: "symbolCT",
         title: "<spring:message code='currency.rate.t'/>",
         required: true,
         width: "100%",
-        valueMap : JSON.parse('${Enum_SymbolCUR}'),
+        valueMap: JSON.parse("${Enum_SymbolCUR}"),
     },
     {
         name: "reference",
         title: "<spring:message code='unit.symbol'/>",
         required: true,
         width: "100%",
-       valueMap : JSON.parse('${Enum_RateReference}')
+        valueMap: JSON.parse("${Enum_RateReference}"),
     },
     {
         name: "currencyRateValue",
         title: "<spring:message code='rate.title'/>",
         required: true,
-        width: "100%" ,
+        width: "100%",
         length: "8",
-    }
+        keyPressFilter: "[0-9]",
+    },
 ]);
 Object.assign(currencyRateTab.listGrid.fields, currencyRateTab.dynamicForm.fields);
 nicico.BasicFormUtil.getDefaultBasicForm(currencyRateTab, "/api/currencyRate");
 currencyRateTab.dynamicForm.main.windowWidth = 500;
+
+currencyRateTab.dynamicForm.main.validate = function () {
+    let isValid = this.Super("validate", arguments);
+    let data = currencyRateTab.dynamicForm.main.getValues();
+    if (data.symbolCF != null && data.symbolCF === data.symbolCT) {
+        currencyRateTab.dynamicForm.main.errors["symbolCT"] = "<spring:message code='currencyRate.checkRate'/>";
+        return false;
+    }
+    return isValid;
+};
