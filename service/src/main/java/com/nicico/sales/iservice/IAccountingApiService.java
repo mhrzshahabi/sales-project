4a3903b9c17cc870a39462424368be9a1508140e
ARezaieZR <a.rezaiezr@gmail.com>
2020-09-08 09:32:48 +0430

    No Comment

--- service/src/main/java/com/nicico/sales/iservice/IAccountingApiService.java
+++ service/src/main/java/com/nicico/sales/iservice/IAccountingApiService.java
@@ -2,9 +2,9 @@ package com.nicico.sales.iservice;
 
 import org.springframework.util.MultiValueMap;
 
-import java.io.IOException;
-
 public interface IAccountingApiService {
 
-    void sendDataParameter(MultiValueMap<String, String> requestParams) throws IOException;
+    void sendDataParameter(MultiValueMap<String, String> requestParams);
+
+    void getDetailCodeInfo(String detailCode);
 }
--- service/src/main/java/com/nicico/sales/service/AccountingApiService.java
+++ service/src/main/java/com/nicico/sales/service/AccountingApiService.java
@@ -9,7 +9,8 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,7 +26,7 @@ public class AccountingApiService implements IAccountingApiService {
     private final OAuth2RestTemplate restTemplate;
 
     @Override
-    public void sendDataParameter(MultiValueMap<String, String> requestParams) throws IOException {
+    public void sendDataParameter(MultiValueMap<String, String> requestParams) {
         final String url = accountingAppUrl + "/rest/system-parameter/addSystemParmeter/" + appName + "/فروش";
         final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -34,6 +35,25 @@ public class AccountingApiService implements IAccountingApiService {
 
         final ResponseEntity<String> httpResponse = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
         if (httpResponse.getStatusCode().equals(HttpStatus.CREATED)) {
+            System.out.println(httpResponse.getBody());
+        } else {
+            throw new RuntimeException("Exception: [" + httpResponse.getStatusCode() + "]: " + httpResponse.getBody());
+        }
+    }
+
+    @Override
+    public void getDetailCodeInfo(String detailCode) {
+        final String url = accountingAppUrl + "/rest/detail/getDetailByCode";
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        final Map<String, String> requestParam = new HashMap<>();
+        requestParam.put("detailCode", detailCode);
+
+        final HttpEntity<Map<String, String>> httpEntity = new HttpEntity<>(requestParam, httpHeaders);
+
+        final ResponseEntity<String> httpResponse = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+        if (httpResponse.getStatusCode().equals(HttpStatus.OK)) {
             System.out.println(httpResponse.getBody());
         } else {
             throw new RuntimeException("Exception: [" + httpResponse.getStatusCode() + "]: " + httpResponse.getBody());
