ea308b52b96f4453075e5cf67529f854d0fdafe1
mshahabi <m@m.com>
2020-11-09 20:43:06 +0330

    BugFixes

--- model/src/main/java/com/nicico/sales/model/enumeration/InspectionRateValueType.java
+++ model/src/main/java/com/nicico/sales/model/enumeration/InspectionRateValueType.java
@@ -7,24 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum InspectionRateValueType {
 
-//    ManPerDay(1),
-//    PerTon(2);
-//
-//    private final Integer id;
-
-    ManPerDay(1, "ManPerDay", "نفر روز"),
-    PerTon(2, "PerTon", "تن");
+    ManPerDay(1),
+    PerTon(2);
 
     private final Integer id;
-    private final String nameEN;
-    private final String nameFA;
 
-
-    public String getNameEN() {
-        return this.nameEN;
-    }
-
-    public String getNameFA() {
-        return this.nameFA;
-    }
 }
