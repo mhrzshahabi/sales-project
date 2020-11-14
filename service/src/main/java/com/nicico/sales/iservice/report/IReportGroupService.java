84e6461a2bdc071d30a9edf3e410ce09c27ab6bc
alikarimi <ali@karimi.com>
2020-11-14 11:49:10 +0330

    BugFix

--- service/src/main/java/com/nicico/sales/iservice/report/IReportGroupService.java
+++ service/src/main/java/com/nicico/sales/iservice/report/IReportGroupService.java
@@ -1,9 +1,10 @@
 package com.nicico.sales.iservice.report;
 
-
 import com.nicico.sales.dto.report.ReportGroupDTO;
 import com.nicico.sales.iservice.IGenericService;
 import com.nicico.sales.model.entities.report.ReportGroup;
+
+import java.util.List;
 
 public interface IReportGroupService extends IGenericService<ReportGroup, Long, ReportGroupDTO.Create, ReportGroupDTO.Info, ReportGroupDTO.Update, ReportGroupDTO.Delete> {
 
