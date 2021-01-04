f7a8510781a94ef1ed7a68e8b0221650f7465605
alikarimi <ali@karimi.com>
2021-01-04 11:29:43 +0330

    IMPROVE

--- controller/src/main/java/com/nicico/sales/controller/EnumRestController.java
+++ controller/src/main/java/com/nicico/sales/controller/EnumRestController.java
@@ -20,143 +20,70 @@ import java.util.ArrayList;
 @RequestMapping(value = "/api/enum")
 public class EnumRestController {
 
+    private Object createEnumObject(String value) {
+
+        return new Object() {{
+
+            final String id = value;
+            final String name = value;
+        }};
+    }
+
+    private ArrayList<Object> createEnumList(Enum[] enums) {
+
+        ArrayList<Object> array = new ArrayList<>();
+        for (Enum value : enums)
+            array.add(createEnumObject(value.name()));
+
+        return array;
+    }
+
     @Loggable
     @GetMapping(value = "/priceBaseReference")
     public ResponseEntity<ArrayList<Object>> getPriceBaseReference(@RequestParam MultiValueMap<String, String> criteria) {
 
-        ArrayList<Object> priceBaseReferenceArray = new ArrayList<>();
-
-        for (PriceBaseReference value : PriceBaseReference.values()) {
-
-            Object pbEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-            priceBaseReferenceArray.add(pbEnum);
-        }
-
-        return new ResponseEntity<>(priceBaseReferenceArray, HttpStatus.OK);
+        return new ResponseEntity<>(createEnumList(PriceBaseReference.values()), HttpStatus.OK);
     }
 
-    @Loggable
-    @GetMapping(value = "/dataType")
-    public ResponseEntity<ArrayList<Object>> getDataType(@RequestParam MultiValueMap<String, String> criteria) {
-
-        ArrayList<Object> dataTypeArray = new ArrayList<>();
-
-        for (DataType value : DataType.values()) {
-
-            Object dtEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-
-            dataTypeArray.add(dtEnum);
-        }
-
-        return new ResponseEntity<>(dataTypeArray, HttpStatus.OK);
-    }
+//    @Loggable
+//    @GetMapping(value = "/dataType")
+//    public ResponseEntity<ArrayList<Object>> getDataType(@RequestParam MultiValueMap<String, String> criteria) {
+//
+//        return new ResponseEntity<>(createEnumList(DataType.values()), HttpStatus.OK);
+//    }
 
     @Loggable
     @GetMapping(value = "/rateReference")
     public ResponseEntity<ArrayList<Object>> getRateReference(@RequestParam MultiValueMap<String, String> criteria) {
 
-        ArrayList<Object> rateReferenceArray = new ArrayList<>();
-
-        for (RateReference value : RateReference.values()) {
-
-            Object rrEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-
-            rateReferenceArray.add(rrEnum);
-        }
-
-        return new ResponseEntity<>(rateReferenceArray, HttpStatus.OK);
+        return new ResponseEntity<>(createEnumList(RateReference.values()), HttpStatus.OK);
     }
 
     @Loggable
     @GetMapping(value = "/commercialRole")
     public ResponseEntity<ArrayList<Object>> getCommercialRole(@RequestParam MultiValueMap<String, String> criteria) {
 
-        ArrayList<Object> commercialRoleArray = new ArrayList<>();
-
-        for (CommercialRole value : CommercialRole.values()) {
-
-            Object crEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-
-            commercialRoleArray.add(crEnum);
-        }
-
-        return new ResponseEntity<>(commercialRoleArray, HttpStatus.OK);
+        return new ResponseEntity<>(createEnumList(CommercialRole.values()), HttpStatus.OK);
     }
 
     @Loggable
     @GetMapping(value = "/categoryUnit")
     public ResponseEntity<ArrayList<Object>> getCategoryUnit(@RequestParam MultiValueMap<String, String> criteria) {
 
-        ArrayList<Object> categoryUnitArray = new ArrayList<>();
-
-        for (CategoryUnit value : CategoryUnit.values()) {
-
-            Object cuEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-
-            categoryUnitArray.add(cuEnum);
-        }
-
-        return new ResponseEntity<>(categoryUnitArray, HttpStatus.OK);
+        return new ResponseEntity<>(createEnumList(CategoryUnit.values()), HttpStatus.OK);
     }
 
-    @Loggable
-    @GetMapping(value = "/contractDetailTypeReference")
-    public ResponseEntity<ArrayList<Object>> getContractDetailTypeReference(@RequestParam MultiValueMap<String, String> criteria) {
-
-        ArrayList<Object> contractDetailTypeReferenceArray = new ArrayList<>();
-
-        for (ContractDetailTypeReference value : ContractDetailTypeReference.values()) {
-
-            Object cdtrEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-
-            contractDetailTypeReferenceArray.add(cdtrEnum);
-        }
-
-        return new ResponseEntity<>(contractDetailTypeReferenceArray, HttpStatus.OK);
-    }
+//    @Loggable
+//    @GetMapping(value = "/contractDetailTypeReference")
+//    public ResponseEntity<ArrayList<Object>> getContractDetailTypeReference(@RequestParam MultiValueMap<String, String> criteria) {
+//
+//        return new ResponseEntity<>(createEnumList(ContractDetailTypeReference.values()), HttpStatus.OK);
+//    }
 
     @Loggable
     @GetMapping(value = "/symbolUnit")
     public ResponseEntity<ArrayList<Object>> getSymbolUnit(@RequestParam MultiValueMap<String, String> criteria) {
 
-        ArrayList<Object> symbolUnitArray = new ArrayList<>();
-
-        for (SymbolUnit value : SymbolUnit.values()) {
-
-            Object suEnum = new Object(){{
-
-                final String id = value.name();
-                final String name = value.name();
-            }};
-
-            symbolUnitArray.add(suEnum);
-        }
-
-        return new ResponseEntity<>(symbolUnitArray, HttpStatus.OK);
+        return new ResponseEntity<>(createEnumList(SymbolUnit.values()), HttpStatus.OK);
     }
-
 }
