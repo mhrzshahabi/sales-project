84d5d164bea7ecc7a649583016f1fbf37837bd42
alikarimi <ali@karimi.com>
2021-01-06 08:10:19 +0330

    .

--- service/src/main/java/com/nicico/sales/iservice/contract/IContractContactService.java
+++ service/src/main/java/com/nicico/sales/iservice/contract/IContractContactService.java
@@ -1,40 +1,11 @@
 package com.nicico.sales.iservice.contract;
 
-import com.nicico.copper.common.domain.criteria.NICICOCriteria;
-import com.nicico.copper.common.dto.grid.TotalResponse;
-import com.nicico.copper.common.dto.search.SearchDTO;
 import com.nicico.sales.dto.contract.ContractContactDTO;
+import com.nicico.sales.iservice.IGenericService;
+import com.nicico.sales.model.entities.contract.ContractContact;
 import com.nicico.sales.model.enumeration.CommercialRole;
 
-import java.util.List;
-
-public interface IContractContactService {
-
-    ContractContactDTO.Info get(Long id);
-
-    List<ContractContactDTO.Info> getAll(List<Long> ids);
-
-    List<ContractContactDTO.Info> list();
-
-    ContractContactDTO.Info create(ContractContactDTO.Create request);
-
-    List<ContractContactDTO.Info> createAll(List<ContractContactDTO.Create> requests);
-
-    ContractContactDTO.Info update(ContractContactDTO.Update request);
-
-    ContractContactDTO.Info update(Long id, ContractContactDTO.Update request);
-
-    List<ContractContactDTO.Info> updateAll(List<ContractContactDTO.Update> requests);
-
-    List<ContractContactDTO.Info> updateAll(List<Long> ids, List<ContractContactDTO.Update> requests);
-
-    void delete(Long id);
-
-    void deleteAll(ContractContactDTO.Delete request);
-
-    TotalResponse<ContractContactDTO.Info> search(NICICOCriteria request);
-
-    SearchDTO.SearchRs<ContractContactDTO.Info> search(SearchDTO.SearchRq request);
+public interface IContractContactService extends IGenericService<ContractContact, Long, ContractContactDTO.Create, ContractContactDTO.Info, ContractContactDTO.Update, ContractContactDTO.Delete> {
 
     ContractContactDTO.Info getByContractIdAndContactIdAndCommercialRole(Long contractId, Long contactId, CommercialRole commercialRole);
 }
